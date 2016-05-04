ef88c8fbdc61eee281b94f23631e32e3b381fa44
multit <andrea.fiorelli@gmail.com>
2016-05-04 16:58:03 +0200

    Inserita pagina archive.php per le liste di categorie e keywords

--- wp-content/themes/cnsg/views/lista-progetti.php
+++ wp-content/themes/cnsg/views/lista-progetti.php
@@ -1,1 +1,3 @@
-<a href="<?php echo get_post_permalink( ); ?>"><?php echo get_the_title( ); ?></a><br>
+<div class="columns large-3 end">
+<a href="<?php echo get_post_permalink( ); ?>"><h2><?php echo get_the_title( ); ?></h2></a><br>
+</div>
